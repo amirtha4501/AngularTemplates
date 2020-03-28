578ce5caf62234493969e61ab3f9de4f423f9893
Amirtha <amirtha4501@gmail.com>
2020-03-28 22:58:58 +0530

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TemplateSyntax';
+  title = 'Template Syntax';
+  text = 'My hero';
+  isUnchanged = true;
+  classes = 'special';
+  parentItem = 'lamp';
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  currentItems = [{
+    id: 21,
+    name: 'phone'
+  },];
+  itemImageUrl1 = '../assets/image/hero.png';
+  itemImageUrl2 = '../assets/image/villain.png';
+  getVal() {
+    return 4;
+  }
+  deleteHero(hero?: Hero) {
+    alert(`Delete ${hero ? hero.name : 'the hero'}.`);
+  }
+  onSubmit(data: any) {/* referenced but not used */}
 }
---
+++ src/app/mock-item.spec.ts
@@ -0,0 +1,7 @@
+import { MockItem } from './mock-item';
+
+describe('MockItem', () => {
+  it('should create an instance', () => {
+    expect(new MockItem()).toBeTruthy();
+  });
+});
